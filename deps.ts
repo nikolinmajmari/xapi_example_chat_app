ca7be9cb42f255707472f3aa98eb2737659655cf
Nikolin Majmari <nikolinmajmari30@gmail.com>
2022-05-03 17:04:30 +0200

    updated deps

--- deps.ts
+++ deps.ts
@@ -3,7 +3,6 @@
 //export {SessionProvider, FileAdapter} from "../../xapi/session/mod.ts";
 //export {type AuthenticableInterface,Authenticable,SessionAuthContext,SessionAuth,authExtractor} from "../../xapi/security/mod.ts";
 
-
 export {Application,Router,engines,staticMiddleware,XapiFormDataFiles,xapiDefaultFileManager, FileManager}
 from "https://raw.githubusercontent.com/nikolinmajmari/xapi/main/xapi/app/mod.ts";
 export {SessionProvider, FileAdapter} from "https://raw.githubusercontent.com/nikolinmajmari/xapi/main/xapi/session/mod.ts";
